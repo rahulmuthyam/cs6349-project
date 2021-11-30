6032d186f867f23adbbab59ccaba0d1cc3b94031
Rahul Muthyam <rahulmuthyam@supergiant.dev>
2021-11-30 12:19:23 -0600

    [all] update logs [test] update

---
+++ test/test-one.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Cleanup
+rm -rf ./temp
+mkdir ./temp
+
+# Run Server
+./run.sh server &
+sleep 5
+
+# Client Upload
+./run.sh upload ./files/archive.zip
+
+# Client Download
+./run.sh download ./temp/archive.zip
+
+# Kill all child processes
+echo -e "\nKilling all child processes..."
+kill 0
+exit 0
